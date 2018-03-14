469272497d454e4766c2ca94606c6e2e1612520f
Administrator <fanwenwu@interheart.cn>
2018-03-14 14:39:48 +0800

    搭建框架

---
+++ common-service-facade/src/main/java/com/fww/commonservicefacade/CommonServiceFacadeApplication.java
@@ -0,0 +1,12 @@
+package com.fww.commonservicefacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonServiceFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonServiceFacadeApplication.class, args);
+	}
+}
