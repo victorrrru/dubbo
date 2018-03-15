5b06a689f67ecda71f98bc633a06e11c6f9de1d9
fww <fanandyou@sina.cn>
2018-03-15 17:58:56 +0800

    添加时间封装类

--- common-service-facade/src/main/java/com/fww/commonservicefacade/common/TimeUtils.java
+++ common-service-facade/src/main/java/com/fww/commonservicefacade/common/TimeUtils.java
@@ -1,9 +1,48 @@
 package com.fww.commonservicefacade.common;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * @author Administrator
  * @date 2018/03/15 16:11
  */
 public class TimeUtils {
 
+    public static final long MILLIS_PER_SECOND = 1000L;
+    public static final long MILLIS_PER_MINUTE = 60000L;
+    public static final long MILLIS_PER_HOUR = 3600000L;
+    public static final long MILLIS_PER_DAY = 86400000L;
+
+    public static Date addYear(Date date, Integer amount) {
+        Calendar instance = add(date, Calendar.YEAR, amount);
+        return instance.getTime();
+    }
+    public static Date addMonth(Date date, Integer amount) {
+        Calendar instance = add(date, Calendar.MONTH, amount);
+        return instance.getTime();
+    }
+    public static Date addDay(Date date, Integer amount) {
+        Calendar instance = add(date, Calendar.DAY_OF_MONTH, amount);
+        return instance.getTime();
+    }
+    public static Date addHour(Date date, Integer amount) {
+        Calendar instance = add(date, Calendar.HOUR_OF_DAY, amount);
+        return instance.getTime();
+    }
+    public static Date addMinute(Date date, Integer amount) {
+        Calendar instance = add(date, Calendar.MINUTE, amount);
+        return instance.getTime();
+    }
+    public static Date addSecond(Date date, Integer amount) {
+        Calendar instance = add(date, Calendar.SECOND, amount);
+        return instance.getTime();
+    }
+
+    private static Calendar add(Date date, Integer dateType, Integer amount) {
+        Calendar instance = Calendar.getInstance();
+        instance.setTime(date);
+        instance.add(dateType, amount);
+        return instance;
+    }
 }
